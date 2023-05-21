1b82154f02adeb2e01de3701448eb5ace44a2367
William Whorton <wwhorton@protonmail.com>
2023-05-21 19:29:30 -0400

    Implementing drag behavior on list

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -5,12 +5,27 @@ import { removeToDo } from "../toDoSlice";
 
 const ToDoItem = (item: IToDoItem) => {
   const dispatch = useAppDispatch();
+  let classes: Set<string> = new Set(["todo-list-item"]);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     dispatch(removeToDo(item));
   };
+  const handleDragStart = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+    classes.add("todo-list-item--dragging");
+  };
+  const handleDragEnd = (event: React.DragEvent<HTMLElement>): void => {
+    event.preventDefault();
+    classes.delete("todo-list-item--dragging");
+  };
+
   return (
-    <li>
+    <li
+      draggable="true"
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      className={Array.from(classes).join(" ")}
+    >
       {item.id} {item.text}
       <button type="button" onClick={handleClick}>
         Delete
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,10 +2,32 @@ import ToDoItem from "./ToDoItem";
 import { IToDoList } from "../interfaces";
 
 const ToDoList = ({ list }: IToDoList) => {
+  const handleDragOver = (event: React.DragEvent<HTMLElement>): void => {
+    console.log(`Drag Over: ${event}`);
+  };
+  const handleDragEnter = (event: React.DragEvent<HTMLElement>): void => {
+    console.log(`Drag Enter: ${event}`);
+  };
+  const handleDragLeave = (event: React.DragEvent<HTMLElement>): void => {
+    console.log(`Drag Leave: ${event}`);
+  };
+  const handleDrop = (event: React.DragEvent<HTMLElement>): void => {
+    console.log(`Drop: ${event}`);
+  };
   return (
     <ul>
       {list.map((item) => {
-        return <ToDoItem id={item.id} text={item.text} key={item.id} />;
+        return (
+          <div
+            className="list-slot"
+            onDragOver={handleDragOver}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+          >
+            <ToDoItem id={item.id} text={item.text} key={item.id} />
+          </div>
+        );
       })}
     </ul>
   );
